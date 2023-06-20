8b565d9b130e4125dfacd6b4990c254b3cf18c50
Gabriel <gabrielvitor083@gmail.com>
2023-06-20 03:05:15 -0300

    First commit

---
+++ src/main/java/com/example/MyProjectSpring/MyProjectSpringApplication.java
@@ -0,0 +1,16 @@
+package com.example.MyProjectSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+@EntityScan (basePackages = "com.example.MyProjectSpring.models")
+@SpringBootApplication
+public class MyProjectSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyProjectSpringApplication.class, args);
+    }
+
+
+}
