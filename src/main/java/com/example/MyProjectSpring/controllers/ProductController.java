1b5af648647d1f11cf854f3b21c853390d00ac40
Gabriel <gabrielvitor083@gmail.com>
2023-06-23 00:55:21 -0300

    Post Method OK

--- src/main/java/com/example/MyProjectSpring/controllers/ProductController.java
+++ src/main/java/com/example/MyProjectSpring/controllers/ProductController.java
@@ -14,9 +14,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ProductController {
-
     @Autowired
     ProductRepository productRepository;
+
 
     @PostMapping("/products")
     public ResponseEntity<ProductModel> saveProduct(@RequestBody @Valid ProductRecordDTO productRecordDTO){
