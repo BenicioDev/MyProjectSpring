4ee28d7d2a40abea9c873a1a5b7d6ba0ed2dbcb6
Gabriel <gabrielvitor083@gmail.com>
2023-06-24 01:00:39 -0300

    Correction Database

--- src/main/java/com/example/MyProjectSpring/controllers/ProductController.java
+++ src/main/java/com/example/MyProjectSpring/controllers/ProductController.java
@@ -4,25 +4,37 @@ import com.example.MyProjectSpring.dtos.ProductRecordDTO;
 import com.example.MyProjectSpring.models.ProductModel;
 import com.example.MyProjectSpring.repositories.ProductRepository;
 import jakarta.validation.Valid;
+import jdk.dynalink.linker.LinkerServices;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class ProductController {
     @Autowired
     ProductRepository productRepository;
 
-
+    //Salvar no BD
     @PostMapping("/products")
     public ResponseEntity<ProductModel> saveProduct(@RequestBody @Valid ProductRecordDTO productRecordDTO){
         var productModel = new ProductModel();
         BeanUtils.copyProperties(productRecordDTO, productModel);
         return ResponseEntity.status(HttpStatus.CREATED).body(productRepository.save(productModel));
     }
+
+    //Listar produtos salvos no BD
+    @GetMapping("/products")
+    public ResponseEntity<List<ProductModel>> getAllProducts(){
+        return ResponseEntity.status(HttpStatus.OK).body(productRepository.findAll());
+    }
+
+    //Listar apenas um produto
 
 }
