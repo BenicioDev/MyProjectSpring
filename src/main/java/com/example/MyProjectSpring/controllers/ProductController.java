0474a6a7e9c763380854001ed362f93f8e56cf9a
Gabriel <gabrielvitor083@gmail.com>
2023-06-25 00:45:38 -0300

    Method delete implemented

--- src/main/java/com/example/MyProjectSpring/controllers/ProductController.java
+++ src/main/java/com/example/MyProjectSpring/controllers/ProductController.java
@@ -4,16 +4,14 @@ import com.example.MyProjectSpring.dtos.ProductRecordDTO;
 import com.example.MyProjectSpring.models.ProductModel;
 import com.example.MyProjectSpring.repositories.ProductRepository;
 import jakarta.validation.Valid;
-import jdk.dynalink.linker.LinkerServices;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.objenesis.SpringObjenesis;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Objects;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -24,7 +22,7 @@ public class ProductController {
 
     //Salvar no BD
     @PostMapping("/products")
-    public ResponseEntity<ProductModel> saveProduct(@RequestBody @Valid ProductRecordDTO productRecordDTO){
+    public ResponseEntity<ProductModel> saveProduct(@RequestBody @Valid ProductRecordDTO productRecordDTO) {
         var productModel = new ProductModel();
         BeanUtils.copyProperties(productRecordDTO, productModel);
         return ResponseEntity.status(HttpStatus.CREATED).body(productRepository.save(productModel));
@@ -32,30 +30,41 @@ public class ProductController {
 
     //Listar produtos salvos no BD
     @GetMapping("/products")
-    public ResponseEntity<List<ProductModel>> getAllProducts(){
+    public ResponseEntity<List<ProductModel>> getAllProducts() {
         return ResponseEntity.status(HttpStatus.OK).body(productRepository.findAll());
     }
 
     //Listar apenas um produto
     @GetMapping("/products/{id}")
-    public ResponseEntity<Object> getOneProduct (@PathVariable (value = "id") UUID id){
-        Optional<ProductModel> product0 = productRepository.findById(id);
-            if (product0.isEmpty()) { return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Produto não encontrado"); }
-            return ResponseEntity.status(HttpStatus.OK).body(product0.get());
+    public ResponseEntity<Object> getOneProduct(@PathVariable(value = "id") UUID id) {
+        Optional<ProductModel> product0 = productRepository.findById(id); // recebendo do BD
+        if (product0.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Produto não encontrado");
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(product0.get());
     }
 
     //Update Produto
     @PutMapping("/products/{id}")
-    public ResponseEntity<Object> updateProduct (@PathVariable (value = "id") UUID id, @RequestBody @Valid
-                                                 ProductRecordDTO productRecordDTO){
-        Optional<ProductModel> product0 = productRepository.findById(id); //Busca BD
-        if (product0.isEmpty()) { return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Produto não encontrado"); }
+    public ResponseEntity<Object> updateProduct(@PathVariable(value = "id") UUID id, @RequestBody @Valid
+    ProductRecordDTO productRecordDTO) {
+        Optional<ProductModel> product0 = productRepository.findById(id); //Busca BD por id do produto
+        if (product0.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Produto não encontrado");
+        }
         var productModel = product0.get();
-        BeanUtils.copyProperties(productRecordDTO, productModel);
-
-
-
-
+        BeanUtils.copyProperties(productRecordDTO, productModel); //converte Dado → DTO → Objeto
+        return ResponseEntity.ok().body(productRepository.save(productModel));
+    }
+    
+    @DeleteMapping("/products/{id}")
+    public ResponseEntity<Object> deleteProduct (@PathVariable(value = "id") UUID id){
+        Optional<ProductModel> product0 = productRepository.findById(id);
+        if (product0.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Produto não encontrado no bando de dacos");
+        }
+        productRepository.delete(product0.get());
+        return  ResponseEntity.status(HttpStatus.OK).body("Produto deletado com sucesso.");
     }
 
 
